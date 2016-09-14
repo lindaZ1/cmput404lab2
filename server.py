71450390ce9e4c7a8b6c637293d583058e01234b
lindaZ1 <dzhang4@ualberta.ca>
2016-09-14 14:34:42 -0600

    client, server

---
+++ server.py
@@ -0,0 +1,13 @@
+import socket
+
+#Allocate a new socket
+server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#Listen on port 8000
+server.bind(('0.0.0.0', 8000))
+server.listen(1)
+
+print "Waiting for connections..."
+client, address=server.accept()
+print "Connected!"
+print address
